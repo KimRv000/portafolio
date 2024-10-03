97bfc3f349bf846e69032d9e1daea75c6c46d2e8
Kimberly Rv <elliis.rv@gmail.com>
2024-10-03 16:09:00 -0600

    [ADD] projects

--- main.js
+++ main.js
@@ -32,3 +32,67 @@ phone_icon.addEventListener("click", () => {
             });
     }
 });
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const projectBirds = document.getElementById('projectBirds');
+    const projectTextBirds = document.getElementById('projectTextBirds');
+
+    function showText() {
+        projectTextBirds.classList.add('visible');
+    }
+
+    function hideText() {
+        projectTextBirds.classList.remove('visible');
+    }
+
+    projectBirds.addEventListener('mouseover', showText);
+
+    projectBirds.addEventListener('mouseleave', hideText);
+});
+*/
+document.getElementById('projectBirds').addEventListener('click', function() {
+    window.open('https://kimrv000.github.io/birds/', '_blank');
+});
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const projectTailwind = document.getElementById('projectTailwind');
+    const projectTextTailwind = document.getElementById('projectTextTailwind');
+
+    function showText() {
+        projectTextTailwind.classList.add('visible');
+    }
+
+    function hideText() {
+        projectTextTailwind.classList.remove('visible');
+    }
+
+    projectTailwind.addEventListener('mouseover', showText);
+
+    projectTailwind.addEventListener('mouseleave', hideText);
+});
+*/
+document.getElementById('projectTailwind').addEventListener('click', function() {
+    window.open('https://kimrv000.github.io/tailwind_travels/', '_blank');
+});
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const projectTicTacToe = document.getElementById('projectTicTacToe');
+    const projectTextTicTacToe = document.getElementById('projectTextTicTacToe');
+
+    function showText() {
+        projectTextTicTacToe.classList.add('visible');
+    }
+
+    function hideText() {
+        projectTextTicTacToe.classList.remove('visible');
+    }
+
+    projectTicTacToe.addEventListener('mouseover', showText);
+
+    projectTicTacToe.addEventListener('mouseleave', hideText);
+});
+*/
+document.getElementById('projectTicTacToe').addEventListener('click', function() {
+    window.open('https://kimrv000.github.io/tic-tac-toe/', '_blank');
+});
